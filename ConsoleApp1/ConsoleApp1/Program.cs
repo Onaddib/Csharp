e20b340813547be5b9448404832f285e121d3d96
Onaddib <73098897+Onaddib@users.noreply.github.com>
2023-08-05 02:28:39 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+    Console.WriteLine("Please enter your word");
+    string w = Console.ReadLine();
+    //Console.WriteLine($"Your word: {w}");
+
+
+    char[] reverseArray = w.ToCharArray();
+    Array.Reverse(reverseArray);
+    string reverseWord = new string(reverseArray);
+
+
+
+    if (w == reverseWord)
+    {
+        Console.WriteLine($"Word {w} is a palindrome word");
+    }
+    else
+    {
+        Console.WriteLine($"Word ${w} is not polindrome");
+    }
+
+
+
+    //Console.WriteLine(reverseWord.GetType());
+
+
+
+
+
+
+
+
+
+
